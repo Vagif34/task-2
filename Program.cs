93e3bca4f043ae3204aa79bde0e8162af0b0eccb
unknown <vagifrg@code.edu.az>
2022-08-02 19:07:19 +0400

    creat file

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace home_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "mart";
+            switch (month)
+            {
+                case "yanvar":
+                case "mart":
+                case "dekabr":
+                case "iyul":
+                    
+                    Console.WriteLine("31 gunden ibaretdir");
+                    break;
+                case "aprel":
+                case "iyun":
+                    case "noyabr"
+                    case "sentyabr":
+                    Console.WriteLine("30 gunden ibaretdir");
+                    break;
+                case "fevral":
+                    Console.WriteLine("28 gunden ibaretdir");
+                    break;
+
+            }
+        }
+    }
+}
